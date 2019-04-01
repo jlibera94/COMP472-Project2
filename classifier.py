1d07ef877a020282551f5ce8aee657d3bf191dc2
jlibera94 <jlibera94@hotmail.com>
2019-04-01 17:41:27 -0400

    retrieving data

--- classifier.py
+++ classifier.py
@@ -11,10 +11,35 @@ import sys
 import re
 import matplotlib
 
-
-f=open("test/testfile.txt", "r")print(f.read())
-
 #loop through all the files 
+def readFiles(path):
+    for root, dirnames, filenames in os.link(path):
+        for filename in filenames:
+            path = os.path.join(root, filename)
+            
+            inBody = False
+            lines = []
+            f = io.open(path, 'r', encoding='latin1')
+            for line in f:
+                if inBody:
+                    lines.append(line)
+                elif line == '\n':
+                    inBody = True
+            f.close()
+            message = '\n' + join(lines)
+            yield path, message
+            
+def dataFrameFromDirectory(path, classification):
+    rows = []
+    index = []
+    for filename, message in readFiles(path):
+        rows.append(['message':message, 'class':classification])
+        index.append(filename)
+        
+    return DataFrame(rows, index=index)
+
+data = DataFrame(['message': [], 'class':[]])
+
 #print-filter through relevant words
 
 #naive bayes
